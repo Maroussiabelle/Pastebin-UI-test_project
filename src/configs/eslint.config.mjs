54246f67ddf3865a6d5db02b2b0e2ed940b80726
Marija Voroncova <marijagrigule@gmail.com>
2024-05-24 12:53:34 +0300

    installed and configured ESLint

---
+++ src/configs/eslint.config.mjs
@@ -0,0 +1,23 @@
+import wdio from 'eslint-plugin-wdio';
+import google from 'eslint-config-google';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...wdio.configs.recommended.globals,
+      },
+    },
+    plugins: {
+      wdio,
+    },
+    rules: {
+      ...google.rules,
+      ...wdio.configs.recommended.rules,
+      'valid-jsdoc': 'off',
+      'require-jsdoc': 'off',
+      'max-len': 'off',
+      'space-in-parens': ['error', 'never'],
+    },
+  },
+];
--- test/data/testData.js
+++ test/data/testData.js
@@ -1,6 +1,6 @@
 export const TASK1_TEST_DATA = {
-    codeToPaste: 'Hello from WebDriver',
-    pasteExpiration: "10 Minutes",
-    pasteName: "helloweb",
-    pasteExpirationResult: "10 min"
-}
+  codeToPaste: 'Hello from WebDriver',
+  pasteExpiration: '10 Minutes',
+  pasteName: 'helloweb',
+  pasteExpirationResult: '10 min',
+};
