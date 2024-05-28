714eb82e1dab45e5786dc5489ae5585d82c05a98
Marija Voroncova <marijagrigule@gmail.com>
2024-05-28 17:11:20 +0300

    added test data for task 3, formatted wdio.conf with ESLint

---
+++ test/data/task3_testData.js
@@ -0,0 +1,10 @@
+export const TASK3_TEST_DATA = {
+  codeToPaste: 'This is a first paste of a registered user',
+  syntaxHighlighting: 'CSS',
+  pasteExpiration: '1 week',
+  pasteName: 'first paste',
+  pasteExpirationResult: '7 days',
+  languageButtonText: 'CSS',
+};
+
+
